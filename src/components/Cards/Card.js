a7351cf8a853686d2a2d9f3bf9751e397d3d810f
Mdakhib <mohamedakhib@gmail.com>
2021-08-02 21:13:02 +0530

    added card by using props

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./cards.css";
 
-export const Card = () => {
+export const Card = ({
+  id,
+  name,
+  description,
+  isVeg,
+  rating,
+  price,
+  img_url,
+}) => {
   return (
-   
+    <div className="cardContainer">
+      <div className="card">
+        <h1>{name} </h1>
+        <img src={img_url} alt={name} width='100px' />
+        <p>{description} </p>
+        <p>{isVeg ? "Veg" : "Non Veg"} </p>
+        <p>{price} </p>
+        <p>{rating} </p>
+        <button>add item</button>
+      </div>
+    </div>
   );
 };
