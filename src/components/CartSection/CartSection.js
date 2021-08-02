9873996c1609aebbd71f329ada8eaab65fa5c7af
Mdakhib <mohamedakhib@gmail.com>
2021-08-03 01:11:24 +0530

    remove item is done by using indexof

--- src/components/CartSection/CartSection.js
+++ src/components/CartSection/CartSection.js
@@ -3,27 +3,34 @@ import { useGlobalContext } from "../../context/context";
 import "./cartsection.css";
 
 function CartSection() {
-  const { dispatch, state } = useGlobalContext();
+    const { dispatch, state } = useGlobalContext();
 
-  return (
-    <div className="cartContainer">
-      {state.basket.map((item) => {
-        const { id, name, img_url, description, isVeg, price, rating } = item;
-        console.log(item);
-        return (
-          <div className="cart" key={id}>
+    const removeFromBasket = (id) => {
+      dispatch({
+        type: "REMOVE_ITEM",
+        payload: id,
+      });
+    };
+
+    return (
+      <div className="cartContainer">
+        {state.basket.map((item) => {
+          const { id, name, img_url, description, isVeg, price, rating } = item;
+          console.log(item);
+          return (
+            <div className="cart" key={id}>
               <h1>{name} </h1>
               <img src={img_url} alt={name} width="100px" />
               <p>{description} </p>
               <p>{isVeg ? "Veg" : "Non Veg"} </p>
               <p>{price} </p>
               <p>{rating} </p>
-              <button>remove</button>
+              <button onClick={() => removeFromBasket(id)}>remove</button>
             </div>
-        );
-      })}
-    </div>
-  );
+          );
+        })}
+      </div>
+    );
 }
 
 export default CartSection;
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -6,6 +6,18 @@ const reducer = (state, action) => {
     case "ADD_ITEM":
       return { ...state, basket: [...state.basket, action.payload] };
 
+    case "REMOVE_ITEM":
+      let newBasket = [...state.basket];
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.payload
+      );
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.log("Cant remove item pls try again");
+      }
+      return { ...state, basket: newBasket };
+
     default:
       return state;
   }
