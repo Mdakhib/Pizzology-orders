4dcae3de1652890ca2d2f56297bce7dab2c200d7
Mdakhib <mohamedakhib@gmail.com>
2021-08-03 10:31:00 +0530

    added empty basket by using ternary

--- src/components/CartSection/CartSection.js
+++ src/components/CartSection/CartSection.js
@@ -12,23 +12,40 @@ function CartSection() {
       });
     };
 
+    const clearBasket = () => {
+      dispatch({
+        type: "CLEAR_CART",
+      });
+    };
+
     return (
-      <div className="cartContainer">
-        {state.basket.map((item) => {
-          const { id, name, img_url, description, isVeg, price, rating } = item;
-          console.log(item);
-          return (
-            <div className="cart" key={id}>
-              <h1>{name} </h1>
-              <img src={img_url} alt={name} width="100px" />
-              <p>{description} </p>
-              <p>{isVeg ? "Veg" : "Non Veg"} </p>
-              <p>{price} </p>
-              <p>{rating} </p>
-              <button onClick={() => removeFromBasket(id)}>remove</button>
-            </div>
-          );
-        })}
+      <div>
+        {state.basket.length === 0 ? (
+          <h1>Your basket is empty</h1>
+        ) : (
+          <div className="cartContainer">
+            {state.basket.map((item) => {
+              const { id, name, img_url, description, isVeg, price, rating } =
+                item;
+              console.log(item);
+              return (
+                <div className="cart" key={id}>
+                  <h1>{name} </h1>
+                  <img src={img_url} alt={name} width="100px" />
+                  <p>{description} </p>
+                  <p>{isVeg ? "Veg" : "Non Veg"} </p>
+                  <p>{price} </p>
+                  <p>{rating} </p>
+                  <button onClick={() => removeFromBasket(id)}>remove</button>
+                </div>
+              );
+            })}
+          </div>
+        )}
+
+        {state.basket.length >= 1 && (
+          <button onClick={clearBasket}>Clear Item</button>
+        )}
       </div>
     );
 }
